516a8c46e4657f74acedeccf5b28bb58cdcc6a9f
vnikesh <nikeev1991@gmail.com>
2017-11-28 15:34:41 -0600

    Summary html edited

--- wiki/wsgi.py
+++ wiki/wsgi.py
@@ -14,6 +14,7 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wiki.settings")
 
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
